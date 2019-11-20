5b669e17e7ae923236fc6b069fe9839574bf42ac
liaozhongxun <869664233@qq.com>
2019-11-20 17:23:47 +0800

    updata

--- src/data/pagedata/other/page_all_topic.js
+++ src/data/pagedata/other/page_all_topic.js
@@ -2,6 +2,22 @@
 let datas = {
     Data: [
         {
+            name:'viewport',
+            tag:['移动端适配'],
+            jsCode:[{
+                lang:'javascript',code:`
+            <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+            // width    设置viewport宽度，为一个正整数，或字符串‘device-width’
+            // device-width  设备宽度
+            // height   设置viewport高度，一般设置了宽度，会自动解析出高度，可以不用设置
+            // initial-scale    默认缩放比例（初始缩放比例），为一个数字，可以带小数
+            // minimum-scale    允许用户最小缩放比例，为一个数字，可以带小数
+            // maximum-scale    允许用户最大缩放比例，为一个数字，可以带小数
+            // user-scalable    是否允许手动缩放            
+                `
+            }],
+        },
+        {
             name: 'em 与 rem 适配',
             note: '1em 当前元素的fong-size大小 。 因为html默认字体大小时16px,1rem默认为16px, 当html的font-size大小为屏幕宽度时,1rem就等于屏幕宽度',
             tosumup: [
